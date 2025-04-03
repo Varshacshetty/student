9c43a3e93e23752170205ae0f87be43bc25fb32a
varsha <varshashetty2003@gmail.com>
2025-04-03 16:37:06 +0100

    final

--- app/page.js
+++ app/page.js
@@ -23,13 +23,13 @@ export default function Home() {
       {studentList.map((stud, index) => {
         return(
           <div key={index} className="border rounded-lg shadow-md p-4 w-80 bg-white my-3">
-            <h2 className="text-lg font-semibold text-gray-800">{stud.name}</h2>
-            <p className="text-sm text-gray-600"><strong>USN:</strong> {stud.usn}</p>
-            <p className="text-sm text-gray-600"><strong>Email:</strong> {stud.email}</p>
-            <p className="text-sm text-gray-600"><strong>Phone:</strong> {stud.phone}</p>
-            <p className="text-sm text-gray-600"><strong>Address:</strong> {stud.addr}</p>
-            <p className="text-sm text-gray-600"><strong>Age:</strong> {stud.age}</p>
-            <p className="text-sm text-gray-600"><strong>Gender:</strong> {stud.gender}</p>
+            <h2 className="text-lg font-semibold text-gray-800">{stud.NAME}</h2>
+            <p className="text-sm text-gray-600"><strong>USN:</strong> {stud.USN}</p>
+            <p className="text-sm text-gray-600"><strong>Email:</strong> {stud.EMAIL}</p>
+            <p className="text-sm text-gray-600"><strong>Phone:</strong> {stud.PHONE}</p>
+            <p className="text-sm text-gray-600"><strong>Address:</strong> {stud.ADDRESS}</p>
+            <p className="text-sm text-gray-600"><strong>Age:</strong> {stud.AGE}</p>
+            <p className="text-sm text-gray-600"><strong>Gender:</strong> {stud.GENDER}</p>
           </div>
         )
       })}
